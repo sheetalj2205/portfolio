1ac7e844e5a4eb28178e828ea20972d8fd8b6388
Shaikh Minhaj <shaikhumarms999@gmail.com>
2022-10-02 17:57:59 +0530

    Add Portfolio Section

--- js/name.js
+++ js/name.js
@@ -1,10 +1,36 @@
-window.onscroll = function() {
-    var el = document.getElementsByClassName('header')[0];
-    var className = 'small';
-    if (el.classList) {
-      if (window.scrollY > 10)
-        el.classList.add(className);
-      else
-        el.classList.remove(className);
+window.onscroll = function () {
+  var el = document.getElementsByClassName('header')[0];
+  var className = 'small';
+  if (el.classList) {
+    if (window.scrollY > 10)
+      el.classList.add(className);
+    else
+      el.classList.remove(className);
+  }
+};
+
+
+filterSelection("all")
+function filterSelection(c) {
+  var x, i;
+  x = document.getElementsByClassName("column");
+  if (c == "all") c = "";
+
+  for (i = 0; i < x.length; i++) {
+    w3RemoveClass(x[i], "show");
+    if (x[i].className.indexOf(c) > -1) w3AddClass(x[i], "show");
+  }
+}
+
+
+function w3AddClass(element, name) {
+  var i, arr1, arr2;
+  arr1 = element.className.split(" ");
+  arr2 = name.split(" ");
+  for (i = 0; i < arr2.length; i++) {
+    if (arr1.indexOf(arr2[i]) == -1) {
+      element.className += " " + arr2[i];
     }
-  };
+  }
+}
+
